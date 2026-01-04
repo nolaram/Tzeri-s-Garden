044af6495b57a35d9d3ab94a06b5f4fce748495e
nolaram <marloncopino10@gmail.com>
2026-01-03 16:32:50 -0800

    changed title screen and added background audio

--- code/title_screen.py
+++ code/title_screen.py
@@ -26,6 +26,21 @@ class TitleScreen:
         self.fade_alpha = 0
         self.fade_speed = 400  # alpha per second
 
+        # title music (use user-provided file at audio/title_music.mp3)
+        self.music_path = Path('audio/title_music.mp3')
+        if not self.music_path.exists():
+            print(f"Title music not found: {self.music_path}")
+            self.music = None
+        else:
+            try:
+                self.music = pygame.mixer.Sound(str(self.music_path))
+                self.music.set_volume(0.2)
+                self.music.play(loops = -1)
+            except Exception as e:
+                # fail gracefully if audio can't be played
+                print(f"Title music error: {e}")
+                self.music = None
+
 
 
     def input(self):
@@ -41,6 +56,13 @@ class TitleScreen:
             if self.fade_alpha >= 255:
                 self.fade_alpha = 255
                 self.done = True
+                # stop the title music when transition finishes
+                if self.music:
+                    try:
+                        self.music.stop()
+                    except Exception:
+                        pass
+
 
     def draw(self):
         self.display_surface.blit(self.image, (0, 0))
@@ -57,3 +79,5 @@ class TitleScreen:
             fade_surf.set_alpha(int(self.fade_alpha))
             fade_surf.fill((0, 0, 0))
             self.display_surface.blit(fade_surf, (0, 0))
+
+
